160f298173e63b6920091b926a9e8b144801480b
Rafael Lopez <lpz.raf@gmail.com>
2018-08-01 01:09:51 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -55,16 +55,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-      programmer_hash[:dennis_ritchie][:languages.to_s]
+      programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -81,7 +75,12 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for  => "Ruby",
+    :languages => ["LISP", "C"]
+ }
+ 
+ programmer_hash
 end
 
 def changing_alan
